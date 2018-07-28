9eaf207d2b0b69ee5c4323e898b608bc5570c593
Chris Salmon <megalofauna@outlook.com>
2018-07-28 11:58:16 -0500

    basic demo

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ function TinyTabs(tabSelector, options = {}) {
   const tabbedComponent = document.querySelector(tabSelector)
 
   let defaults = {
+    tabList: tabbedComponent.querySelector('ul'),
     tabTrigger: '[data-tab-trigger]',
     tabContent: '[data-tab-content]',
     tabTriggerActiveClass: 'is-active'
@@ -14,12 +15,22 @@ function TinyTabs(tabSelector, options = {}) {
 
   tabContents[0].removeAttribute('hidden')
   tabContents[0].removeAttribute('aria-hidden')
+  applied.tabList.setAttribute('aria-role', 'tablist')
+
+  tabContents.forEach(tabContent => {
+    tabContent.setAttribute('aria-role', 'tabpanel')
+  })
+
+  tabTriggers.forEach(tabTrigger => {
+    tabTrigger.setAttribute('role', 'tab')
+    tabTrigger.parentNode.setAttribute('aria-role', 'presentation')
+  })
 
   tabTriggers.forEach(tabTrigger => {
     tabTrigger.addEventListener('click', e => {
       e.preventDefault();
       let id = tabTrigger.getAttribute('href')
-      let associatedContent = tabbedComponent.querySelector(id)
+      let targetContent = tabbedComponent.querySelector(id)
 
       tabTriggers.forEach(tabTrigger => {
         tabTrigger.classList.remove(applied.tabTriggerActiveClass);
@@ -31,10 +42,17 @@ function TinyTabs(tabSelector, options = {}) {
       })
 
       tabTrigger.classList.add(applied.tabTriggerActiveClass)
-      associatedContent.removeAttribute('hidden')
-      associatedContent.removeAttribute('aria-hidden')
+      targetContent.removeAttribute('hidden')
+      targetContent.removeAttribute('aria-hidden')
     })
   })
+
+  const separator = document.createElement('div');
+  separator.id = 'end-of-tab';
+  separator.setAttribute('role', 'separator');
+  separator.setAttribute('aria-labelledby', 'end-of-tab');
+  separator.setAttribute('aria-label', 'End of');
+  tabbedComponent.appendChild(separator);
 }
 
 
