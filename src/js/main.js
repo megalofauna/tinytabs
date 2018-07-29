4f6599ec249c06c21b0ad7881dd1deccdd09c58a
Chris Salmon <megalofauna@outlook.com>
2018-07-29 10:14:11 -0500

    adds some accesibility features

--- src/js/main.js
+++ src/js/main.js
@@ -15,45 +15,52 @@ function TinyTabs(tabSelector, options = {}) {
 
   tabContents[0].removeAttribute('hidden')
   tabContents[0].removeAttribute('aria-hidden')
+  tabTriggers[0].setAttribute('aria-selected', 'true')
   applied.tabList.setAttribute('aria-role', 'tablist')
-
-  tabContents.forEach(tabContent => {
-    tabContent.setAttribute('aria-role', 'tabpanel')
-    tabContent.setAttribute('aria-hidden', "")
-  })
 
   tabTriggers.forEach(tabTrigger => {
     tabTrigger.setAttribute('role', 'tab')
     tabTrigger.parentNode.setAttribute('aria-role', 'presentation')
   })
 
+  tabContents.forEach(tabContent => {
+    tabContent.setAttribute('aria-role', 'tabpanel')
+    tabContent.setAttribute('aria-hidden', "")
+  })
+
+  const switchTab = (tabTrigger) => {
+    let id = tabTrigger.getAttribute('href')
+    let targetContent = tabbedComponent.querySelector(id)
+
+    tabTriggers.forEach(tabTrigger => {
+      tabTrigger.classList.remove(applied.tabTriggerActiveClass);
+      tabTrigger.removeAttribute('aria-selected')
+    })
+
+    tabContents.forEach(tabContent => {
+      tabContent.setAttribute('hidden', '')
+      tabContent.setAttribute('aria-hidden', 'true')
+    })
+
+    tabTrigger.classList.add(applied.tabTriggerActiveClass)
+    tabTrigger.setAttribute('aria-selected', 'true')
+    targetContent.removeAttribute('hidden')
+    targetContent.removeAttribute('aria-hidden')
+  }
+
   tabTriggers.forEach(tabTrigger => {
     tabTrigger.addEventListener('click', e => {
       e.preventDefault();
-      let id = tabTrigger.getAttribute('href')
-      let targetContent = tabbedComponent.querySelector(id)
-
-      tabTriggers.forEach(tabTrigger => {
-        tabTrigger.classList.remove(applied.tabTriggerActiveClass);
-      })
-
-      tabContents.forEach(tabContent => {
-        tabContent.setAttribute('hidden', '')
-        tabContent.setAttribute('aria-hidden', 'true')
-      })
-
-      tabTrigger.classList.add(applied.tabTriggerActiveClass)
-      targetContent.removeAttribute('hidden')
-      targetContent.removeAttribute('aria-hidden')
+      switchTab(tabTrigger)
     })
   })
 
-  const separator = document.createElement('div');
-  separator.id = 'end-of-tab';
-  separator.setAttribute('role', 'separator');
-  separator.setAttribute('aria-labelledby', 'end-of-tab');
-  separator.setAttribute('aria-label', 'End of');
-  tabbedComponent.appendChild(separator);
+  const separator = document.createElement('div')
+  separator.id = 'end-of-tab'
+  separator.setAttribute('role', 'separator')
+  separator.setAttribute('aria-labelledby', 'end-of-tab')
+  separator.setAttribute('aria-label', 'End of')
+  tabbedComponent.appendChild(separator)
 }
 
 
